96ddc42ad69d16e6b4346cdd6f5a364caa525fe2
sebastian.vm <sebastian.villadam@gmail.com>
2024-03-03 11:18:18 -0500

    feat(private): Boilerplate for URL shortener component

---
+++ src/app/components/private/url-shortener/url-shortener.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-url-shortener',
+  standalone: true,
+  imports: [],
+  templateUrl: './url-shortener.component.html',
+  styleUrl: './url-shortener.component.css'
+})
+export class UrlShortenerComponent {
+
+}
