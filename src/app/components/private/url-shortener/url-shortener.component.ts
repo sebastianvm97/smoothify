881c0532388469000a8b8621b332f8134120f0fe
sebastian.vm <sebastian.villadam@gmail.com>
2024-03-11 20:23:46 -0500

    feat(private): Implements testing codebase for URL shortener component

--- src/app/components/private/url-shortener/url-shortener.component.ts
+++ src/app/components/private/url-shortener/url-shortener.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { PrivateDataService } from '../services/privateData.service';
 
 @Component({
   selector: 'app-url-shortener',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    PrivateDataService
+  ],
   templateUrl: './url-shortener.component.html',
   styleUrl: './url-shortener.component.css'
 })
 export class UrlShortenerComponent {
+
+  URLForm = new FormGroup({
+    urlControl: new FormControl('', Validators.required),
+    needsAuthControl: new FormControl(false, Validators.required)
+  });
+  generatedURL: string = '';
+
+  constructor(
+    private privateDataService: PrivateDataService
+  ) {}
+
+  public async onURLFormSubmit() {
+    const URL = this.URLForm.controls.urlControl.value;
+
+    const hash = Math.random().toString(36).substring(2, 15);
+
+    this.generatedURL = btoa(hash);
+    console.log(atob(this.generatedURL));
+  }
+
+  public onAdd() {
+    try {
+      this.privateDataService.addDoc();
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 }
