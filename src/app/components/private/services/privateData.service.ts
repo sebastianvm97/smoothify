0b41ce8618a2f6d58a550829e1e37f77e067515f
sebastian.vm <sebastian.villadam@gmail.com>
2024-03-11 20:23:23 -0500

    feat(service): Implements testing codebase to private data service

---
+++ src/app/components/private/services/privateData.service.ts
@@ -0,0 +1,42 @@
+import { initializeApp } from "firebase/app";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
+import { firebaseConfig } from "../../../environments/environments";
+import { Injectable } from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+export class PrivateDataService {
+    private app = initializeApp(firebaseConfig);
+    private db = getFirestore(this.app);
+    
+    public async addDoc() {
+        // CREATES A DOC WITH A RANDOM ID
+        // const docRef = await addDoc(collection(this.db, "users"), {
+        //     id: 123456,
+        //     first: "John",
+        //     last: "Doe",
+        //     born: 1995
+        // });
+        // console.log("Document written with ID: ", docRef.id);
+
+
+
+        // CREATES A DOC WITH A DEFINED ID OF 123456
+        // const docRef = await setDoc(doc(this.db, 'users', '123456'), {
+        //     first: 'Alice',
+        //     last: 'Doe',
+        //     born: 1997
+        // });
+        // console.log("Document created");
+
+
+        // CREATES THE URL INSIDE URLS COLLECTION OF THE USER
+        // const docRef = await addDoc(collection(this.db, "users/123456/urls"), {
+        //     url: 'https://google.com',
+        //     expires: '2024-04-04',
+        //     needsValidation: false,
+        //     isQR: false
+        // });
+        // console.log("Document written with ID: ", docRef.id);
+    }
+}
