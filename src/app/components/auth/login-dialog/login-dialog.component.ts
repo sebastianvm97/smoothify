15cc89db277ca4060c206c09bf95beada2f61d45
sebastian.vm <sebastian.villadam@gmail.com>
2024-04-13 11:22:37 -0500

    feat(auth): Boilerplate for Login dialog component

---
+++ src/app/components/auth/login-dialog/login-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-dialog.component.html',
+  styleUrl: './login-dialog.component.css'
+})
+export class LoginDialogComponent {
+
+}
