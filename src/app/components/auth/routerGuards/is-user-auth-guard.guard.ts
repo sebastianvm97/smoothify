3b1cb92da9564144170469b61b6c641478a1f3ea
sebastian.vm <sebastian.villadam@gmail.com>
2024-04-28 13:36:18 -0500

    feat(auth): Implements codebase for is user auth router guard

--- src/app/components/auth/routerGuards/is-user-auth-guard.guard.ts
+++ src/app/components/auth/routerGuards/is-user-auth-guard.guard.ts
@@ -1,5 +1,12 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
 
-export const isUserAuthGuardGuard: CanActivateFn = (route, state) => {
-  return true;
+export const isUserAuthGuardGuard: CanActivateFn = async (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const authStatus = await  authService.isUserAuth();
+
+  return !authStatus ? router.navigate(['/']) : true; 
 };
