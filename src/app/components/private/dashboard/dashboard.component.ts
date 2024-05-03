f4c7301cd4205c7ee8753a6a2d89e7c0cc0025a0
sebastian.vm <sebastian.villadam@gmail.com>
2024-05-02 20:44:02 -0500

    feat(private): Implement URL shortener child component in Dashboard component

--- src/app/components/private/dashboard/dashboard.component.ts
+++ src/app/components/private/dashboard/dashboard.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { UrlShortenerComponent } from '../url-shortener/url-shortener.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    UrlShortenerComponent
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
