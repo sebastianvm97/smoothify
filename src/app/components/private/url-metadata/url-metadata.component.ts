234d3fb5ad87dabe8f1fd80ed4e2914af8b7018e
sebastian.vm <sebastian.villadam@gmail.com>
2024-05-14 20:49:06 -0500

    feat(private): Boilerplate for URL metadata component

---
+++ src/app/components/private/url-metadata/url-metadata.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-url-metadata',
+  standalone: true,
+  imports: [],
+  templateUrl: './url-metadata.component.html',
+  styleUrl: './url-metadata.component.css'
+})
+export class UrlMetadataComponent {
+
+}
