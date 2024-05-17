2436cef938ed727e76a9372da178aae630521f94
sebastian.vm <sebastian.villadam@gmail.com>
2024-05-16 20:08:17 -0500

    feat(public): Boilerplate for Redirection landing component

---
+++ src/app/components/public/redirection-landing/redirection-landing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redirection-landing',
+  standalone: true,
+  imports: [],
+  templateUrl: './redirection-landing.component.html',
+  styleUrl: './redirection-landing.component.css'
+})
+export class RedirectionLandingComponent {
+
+}
