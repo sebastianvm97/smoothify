7ff3a833c456aebd1188e4abd023bc9edd240424
sebastian.vm <sebastian.villadam@gmail.com>
2024-05-16 21:38:48 -0500

    feat(public): Implements redirection logic for Redirection resolver

--- src/app/components/public/resolvers/redirection.resolver.ts
+++ src/app/components/public/resolvers/redirection.resolver.ts
@@ -1,5 +1,20 @@
+import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
+import { PublicService } from '../services/public.service';
+import _moment from 'moment';
 
-export const redirectionResolver: ResolveFn<boolean> = (route, state) => {
+export const redirectionResolver: ResolveFn<boolean> = async (route, state) => {
+  const publicService = inject(PublicService);
+  const URLId = route.params['id'];
+  const currentDate = _moment();
+
+  const URLData = await publicService.getURLData(URLId);
+  const urlExpirationDate = _moment(URLData.expiresIn);
+
+  if(currentDate.isBefore(urlExpirationDate) && !URLData.needsAuth) {
+    window.open(URLData.url, '_self');
+  }
+
+  await new Promise((resolve) => setInterval(() => { resolve(true); }, 1000));
   return true;
 };
