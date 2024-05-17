fcf5f027736e1d955379c65fc1a581f777add7c9
sebastian.vm <sebastian.villadam@gmail.com>
2024-05-16 21:38:22 -0500

    feat(public): Implements function in public service to get a URL data

--- src/app/components/public/services/public.service.ts
+++ src/app/components/public/services/public.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from "../../../environments/environments";
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
+import { URL } from '../../private/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PublicService {
 
-  constructor() { }
+  private app = initializeApp(firebaseConfig);
+  private db = getFirestore(this.app);
+
+  public async getURLData(URLId: string): Promise<URL> {
+    const querySnapshot = await getDoc(doc(this.db, 'urls', URLId));
+
+    return querySnapshot.data() as any;
+  }
 }
